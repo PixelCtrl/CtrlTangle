781caab92834932993c97e55d4bb0d73a7de12ce
hasira09 <hasiramahel09@gmail.com>
2023-10-17 23:12:12 +0530

    Added the Script to the Player Movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //Variables
+
+    public float speed;
+
+    private Rigidbody playerRigidBody;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+
     }
 }
